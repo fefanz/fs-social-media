96a3cd9ab455203c8cf1635f2d3b0fb6b488fbfe
fefanz <166657605+fefanz@users.noreply.github.com>
2025-08-09 23:14:18 +0100

    Create config.js

---
+++ src/src/config.js
@@ -0,0 +1,3 @@
+// Contactos da FS Social Media (edita para os teus)
+export const CONTACT_EMAIL = "hello@fssocial.media";   // ex: felipecfanzeres@gmail.com
+export const WHATSAPP_DEFAULT = "+351000000000";       // ex: +351926146299
